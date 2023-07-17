fad7e6e67a0a756402233b7b48f05408fadadfc7
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-07-17 17:17:51 -0300

    it starts card structure

---
+++ src/app/components/card.tsx
@@ -0,0 +1,12 @@
+
+// interface Iprops {
+//   drinks:
+// }
+// export default function Card(props) {
+//   const {drinks} = props;
+//   return (       
+//     <div className='relative w-[300px] h-[400px] flex justify-center items-center	bg-black before:absolute before:w-[150px] before:h-[140%] before:bg-gradient-to-b from-blue-500 to-fuchsia-800 before:animate-spin-slow overflow-hidden rounded-2xl after:absolute after:inset-1 after:bg-blue-950'>
+//       <h1 className='text-5xl text-white bold relative z-10'>Hello</h1>
+//     </div>
+//   )
+// }
