d9c26780290b309cc8a695fd1794a507a8fed322
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-07-21 14:52:57 -0300

    perf: it removes apicall from useEffect and it adds tanstack to improces performance on card's component

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,8 +1,8 @@
-import { Idrink } from "../interfaces/Drink";
+import { IFilteredDrinks} from "../interfaces/Drink";
 import { useRouter } from "next/navigation";
 
 export interface IAppProps{
-  drinksData: Idrink[]
+  drinksData?: IFilteredDrinks[]
 }
 
 export default function Card({drinksData}: IAppProps) {
@@ -14,7 +14,7 @@ export default function Card({drinksData}: IAppProps) {
 
   return (       
     <div className='flex flex-row w-screen items-center mt-60 flex-wrap justify-center gap-10'>
-    {drinksData.length > 0 ? (drinksData.map(({strDrink, idDrink, strDrinkThumb}) => (
+    {drinksData && (drinksData.map(({strDrink, idDrink, strDrinkThumb}) => (
       <div key={idDrink} className='relative w-[260px] md:w-[300px] h-[450px] flex justify-evenly items-center bg-black before:absolute before:w-[150px] before:h-[140%] before:bg-gradient-to-b from-blue-500 to-fuchsia-800 before:animate-spin-slow overflow-hidden rounded-2xl after:absolute after:inset-1 after:bg-newblue-950 flex-col gap-y-4'>
         <h1 className='md:text-3xl text-2xl text-white bold relative z-10 font-bold w-full text-center px-3'>{strDrink}</h1>
         <div  className='w-full relative z-10 flex items-center justify-center'>
@@ -26,8 +26,6 @@ export default function Card({drinksData}: IAppProps) {
       </div>
     )
     ))
-    :
-    <h1 className='text-white'>Loading ...</h1>
   }
   </div>
   )
