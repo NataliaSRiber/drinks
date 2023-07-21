f980444d58ee6031f6aaa69df619121f10119eb7
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-07-21 14:52:42 -0300

    perf: it removes apicall from useEffect and it adds tanstack to improces performance on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 'use client'
 import { api } from './services/api'
-import { useEffect, useState } from 'react'
-import { Idrink } from './interfaces/Drink'
+import { IFilteredDrinksList, Idrink } from './interfaces/Drink'
 import Card from './components/card'
+import { useQuery } from '@tanstack/react-query'
 
 export default function Home() {
-  const [drinksData, setDrinksData] = useState<Idrink[]>([])
   
-  useEffect(() => {
-    const getByLetter = async () => {
-      const response = await api.get('/search.php?f=a')
-      setDrinksData(response.data.drinks)
-      return response.data.drinks;
-    }
-    getByLetter()    
-  }, [])
+  const getByFilter = async () => {
+    const response = await api.get<IFilteredDrinksList>('filter.php?c=Ordinary_Drink')
+     return response.data.drinks;
+  }
+  
+  const {data}  = useQuery({
+    queryKey: ['filteredDrinks'],
+    queryFn: getByFilter
+  })
 
   return (
-    <main className='bg-newblue-950 h-full flex flex-col items-center justify-center w-full py-10'>
-      <Card drinksData={drinksData}/>
-    </main>
+      <main className='bg-newblue-950 h-full flex flex-col items-center justify-center w-full py-10'>
+        <Card drinksData={data}/>
+      </main>
   )
 }
