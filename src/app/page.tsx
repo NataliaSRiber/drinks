a88d506dd418121e78211241f6e3b554883a3618
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-07-25 18:40:06 -0300

    feat: it adds context and api calls on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
 'use client'
 import { api } from './services/api'
-import { IFilteredDrinksList, Idrink } from './interfaces/Drink'
 import Card from './components/card'
 import { useQuery } from '@tanstack/react-query'
+import { useContext } from "react";
+import { SearchContext } from './contexts/search-context'
 
 export default function Home() {
-  
-  const getByFilter = async () => {
-    const response = await api.get<IFilteredDrinksList>('filter.php?c=Ordinary_Drink')
-     return response.data.drinks;
+  const { search, clickButton, setClickButton, filteredDrinks } = useContext(SearchContext)
+
+  const getDrinks = async () => {
+    if(filteredDrinks) {
+      const response = await api.get<any>(`/filter.php?c=${filteredDrinks}`)
+      setClickButton(false)
+      return response.data.drinks
+    }
+    if(search) {
+      const response = await api.get<any>(`/search.php?s=${search}`)
+      setClickButton(false)
+      return response.data.drinks
+    }
   }
-  
-  const {data}  = useQuery({
-    queryKey: ['filteredDrinks'],
-    queryFn: getByFilter
-  })
+
+  const { data: drinks } = useQuery({
+    queryKey: [ "drinks2" ],
+    queryFn: getDrinks,
+    enabled: clickButton
+  });
 
   return (
-      <main className='bg-newblue-950 h-full flex flex-col items-center justify-center w-full py-10'>
-        <Card drinksData={data}/>
+      <main className='bg-newblue-950 h-full flex flex-col items-center justify-center w-full py-10 mt-44'>
+        <Card drinksData={drinks}/>
       </main>
   )
 }
