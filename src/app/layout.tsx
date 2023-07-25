db3b5b6f391f0762dc32d33b7f977d14c39edbd8
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-07-25 18:38:31 -0300

    feat: it adds provider on layoult

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Nav from './components/nav'
 import './globals.css'
 import { BioRhyme } from 'next/font/google'
 import Providers from './providers'
+import { SearchContextProvider } from './contexts/search-context'
 
 const bioRhyme = BioRhyme({ subsets: ['latin'],  weight: [ '200', '300', '400', '700', '800'], display: 'swap',
 })
@@ -18,11 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={bioRhyme.className}>  
-        <Providers>
-          <Nav />
-          {children}
-        </Providers>
+      <body className={bioRhyme.className}>
+        <SearchContextProvider>
+          <Providers>
+            <Nav />
+            {children}
+          </Providers>
+        </SearchContextProvider>
       </body>
     </html>
   )
