55ea59f6d529c7d28f98b66183ff6bb1fe026e81
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-07-26 14:54:10 -0300

    feat: it starts not found page

---
+++ src/app/components/notFound.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import logo1 from '../assets/newlogo.png'
+import Image from "next/image"
+
+export default function NotFound() {
+  return (
+    <div className="w-full h-screen m-0 p-0 box-border flex flex-col items-center justify-center min-h-full md:gap-y-12 gap-y-4 text-center">
+      <h1 className="font-bold md:drop-shadow-4xl drop-shadow-3xl text-white md:text-6xl text-4xl">404</h1>
+      <h1 className="font-bold md:drop-shadow-4xl drop-shadow-3xl text-white md:text-6xl text-4xl">PAGE NOT FOUND</h1>
+      <Image src={logo1} alt="image"/>
+      {/* <Link href='/'>
+        <button className=" mt-8 bg-greenneon-500 p-2 rounded-md text-newblue-950 font-semibold">
+          GO BACK
+        </button>
+      </Link> */}
+    </div>
+  )
+}
