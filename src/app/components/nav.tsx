9c4ee1afdaa7c3502f85bc58cc3062be0f204be5
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-08-01 11:45:15 -0300

    feat: it adds new colors on nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,14 +1,13 @@
 "use client"
 import Image from "next/image";
-import logo from '../assets/newlogo.png'
+import logo from '../assets/logo1.png'
 import Link from "next/link";
 import { api } from "../services/api";
 import { ICategories } from "../interfaces/Drink";
 import { useQuery } from "@tanstack/react-query";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { SearchContext } from "../contexts/search-context";
 import { useRouter } from "next/navigation";
-
 
 export default function Nav() {
   const { search, setSearch, setFilteredDrinks, setClickButton  } = useContext(SearchContext)
@@ -37,19 +36,19 @@ export default function Nav() {
 
   return (       
     <div className='w-full flex items-center justify-start fixed top-0 h-100 shadow-md bg-newblue-950 z-20 flex-col gap-y-4'>
-      <div className="flex w-full top-0 flex-row items-center justify-center">
+      <div className="flex w-full top-0 md:flex-row items-center justify-center flex-col">
         <Link href='/'>
-          <Image src={logo} alt="logo" width={100} height={100}/>
+          <Image src={logo} alt="logo" className="md:w-[100px] md:h-[100px] w-[50px] h-[50px]"/>
         </Link>
-        <form className="w-full h-16 flex flex-row items-center justify-center" onSubmit={handleClick2}>
+        <form className="w-full h-16 flex flex-row items-center justify-center px-2" onSubmit={handleClick2}>
           <input placeholder='Type a cocktail name'
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className='md:w-1/2 w-10/12 h-12 rounded-xl p-4 lg:w-2/3'/>
+          className='md:w-1/2 w-10/12 h-12 rounded-xl p-4 lg:w-2/3 focus:outline-none'/>
           <button
             disabled={!search}
-            className="flex text-white border-b-4 border border-fuchsia-800 hover:border-blue-500 p-4 rounded-lg cursor-pointer ml-2 h-12 items-center"
+            className="flex disabled:drop-shadow-none disabled:border-white disabled:cursor-auto drop-shadow-4xl text-white border-b-4 border border-pinkneon-500 hover:drop-shadow-6xl hover:border-yellowneon-500 p-4 rounded-lg cursor-pointer ml-2 h-12 items-center"
             type="submit"   
           >
             Search
@@ -57,9 +56,9 @@ export default function Nav() {
         </form>
       </div>
       <div className="w-full">
-        <ul className="flex flex-row text-white border-b-4 border border-blue-500 w-full justify-around h-20 items-center">
+        <ul className="flex flex-row text-white border-b-4 border border-yellowneon-500 w-full justify-evenly h-20 items-center">
           {categories && categories.map((category: any, index: number )=> 
-          <button key={index} className="h-full px-1" onClick={()=> handleClick(category)}>{category.strCategory}</button>
+          <button key={index} className="h-full px-1 hover:text-yellowneon-500 hover:drop-shadow-5xl font-bold" onClick={()=> handleClick(category)}>{category.strCategory}</button>
           )}
         </ul>
       </div>
