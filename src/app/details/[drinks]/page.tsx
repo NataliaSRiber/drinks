05926359d62742f450f98d4a3e96849ffbb75df8
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-08-01 13:08:25 -0300

    feat: it modifies some details

--- src/app/details/[drinks]/page.tsx
+++ src/app/details/[drinks]/page.tsx
@@ -1,10 +1,8 @@
 'use client'
+import BackButton from '@/app/components/backbutton';
 import { api } from '@/app/services/api'
 import { useQuery } from '@tanstack/react-query'
-import Link from 'next/link'
 import { useParams } from 'next/navigation'
-import back from '../../assets/back_arrow.svg'
-import Image from 'next/image'
 
 export default function Details() {
   const getUseParams = useParams();
@@ -22,22 +20,16 @@ export default function Details() {
     const getIngredients = data && (Object.keys(data).filter(v => v.startsWith('strIngredient')).map((strIngredient, index) => (data['strMeasure'.concat(String(index+1))]) + ' '.concat(String(data[strIngredient]))).filter((item)=> item !== 'null null'))
 
     return (       
-    <div className='flex flex-row w-screen items-center mt-72 flex-wrap justify-center gap-10 bg-newblue-950 px-4 md:px-20 pb-20'>
-      <Link href='/'>
-        <button className='text-newblue-950 flex flex-row bg-yellowneon-500 p-2 rounded-lg justify-evenly w-24 cursor-pointer'>
-          <Image src={back} alt='back-arrow'/>
-          Back
-        </button>
-      </Link>
+    <div className='flex flex-row w-screen items-center md:mt-72 mt-64 flex-wrap justify-center gap-10  md:px-20 pb-20 px-5'>
       {data &&
         <div key={data.idDrink} className='w-full h-full text-white'>
           <div className='flex flex-col gap-y-4'>
-            <div className='w-full relative z-10 flex items-center justify-center'>
-              <img src={data.strDrinkThumb} alt='drink-image' className='md:w-1/3 md:h-1/3 rounded-lg w-1/2 h-1/2'/>
+            <div className='w-full relative z-10 flex items-center justify-center drop-shadow-2xl'>
+              <img src={data.strDrinkThumb} alt='drink-image' className='md:w-1/3 md:h-1/3 rounded-lg w-2/3 h-2/3'/>
             </div>
-            <h1 className="md:text-3xl text-2xl text-center  py-4 underline underline-offset-8 decoration-blueneon-500 font-bold">{data.strDrink}</h1>
+            <h1 className="md:text-3xl text-2xl text-center  py-4 underline underline-offset-8 decoration-blueneon-300 font-bold">{data.strDrink}</h1>
             <div className='flex flex-col text-newfuchsia-700 md:text-lg text-lg gap-y-4'>
-              <p className="md:text-lg text-md text-start font-bold text-blueneon-500">{data.strAlcoholic}</p>
+              <p className="md:text-lg text-md text-start font-bold text-blueneon-300">{data.strAlcoholic}</p>
               <div className='flex flex-row'>
                 <p className="md:text-xl text-lg text-start  font-bold">Category:</p>
                 <p className="md:text-xl text-lg text-start  text-white ml-1">{data.strCategory}</p>           
@@ -59,7 +51,8 @@ export default function Details() {
             <p className='text-white md:text-xl text-lg text-justify'>{data.strInstructions}</p>
           </div>
         </div>
-}
+      }
+      <BackButton />
     </div>
   )
 }
