d2795ccade5d030a9e1d3e16ff331095d13995c2
jmlinger <jmlinger10@gmail.com>
2023-08-01 16:31:22 -0300

    feat: css style changes.

--- src/app/components/notFound.tsx
+++ src/app/components/notFound.tsx
@@ -1,13 +1,19 @@
-import Link from "next/link"
+import Link from 'next/link'
 import logo1 from '../assets/newlogo.png'
-import Image from "next/image"
+import Image from 'next/image'
 
 export default function NotFound() {
   return (
-    <div className="w-full h-screen m-0 p-0 box-border flex flex-col items-center justify-center min-h-full md:gap-y-12 gap-y-4 text-center">
-      <h1 className="font-bold md:drop-shadow-4xl drop-shadow-3xl text-white md:text-6xl text-4xl">404</h1>
-      <h1 className="font-bold md:drop-shadow-4xl drop-shadow-3xl text-white md:text-6xl text-4xl">DRINK NOT FOUND</h1>
-      <Image src={logo1} alt="image"/>
+    <div className="absolute left-1/2 top-[70%] flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
+      {/* <h1 className="font-bold md:drop-shadow-4xl drop-shadow-3xl text-white md:text-6xl text-4xl">404</h1> */}
+      <h1 className="top- relative whitespace-nowrap text-xl font-bold text-white drop-shadow-3xl sm:text-4xl md:drop-shadow-4xl xl:text-6xl">
+        DRINKS NOT FOUND
+      </h1>
+      <Image
+        src={logo1}
+        alt="image"
+        className="max-w-[180px] sm:max-w-[240px] xl:max-w-[300px]"
+      />
       {/* <Link href='/'>
         <button className=" mt-8 bg-greenneon-500 p-2 rounded-md text-newblue-950 font-semibold">
           GO BACK
