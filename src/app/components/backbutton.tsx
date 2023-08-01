5c190cf5ad46d18c5a17a3c1ff7cf7fd41ed6dd3
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-08-01 13:06:48 -0300

    feat: it creates back button component for details page

---
+++ src/app/components/backbutton.tsx
@@ -0,0 +1,16 @@
+import back from '../assets/back_arrow.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function BackButton () {
+  return(
+    <div className='fixed bottom-2 right-2'>
+      <Link href='/'>
+        <button className='text-newblue-950 flex flex-row bg-yellowneon-500 drop-shadow-5xl p-2 rounded-lg justify-evenly md:w-24 w-20 cursor-pointer'>
+          <Image src={back} alt='back-arrow'/>
+          Back
+        </button>
+    </Link>
+    </div>
+  )
+}
