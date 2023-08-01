0dc837eead7786dc263ffaf2ec0898249d18dcea
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-08-01 15:49:45 -0300

    chore: it modifies back button

--- src/app/components/backbutton.tsx
+++ src/app/components/backbutton.tsx
@@ -6,7 +6,7 @@ export default function BackButton () {
   return(
     <div className='fixed bottom-2 right-2'>
       <Link href='/'>
-        <button className='text-newblue-950 flex flex-row bg-yellowneon-500 drop-shadow-5xl p-2 rounded-lg justify-evenly md:w-24 w-20 cursor-pointer'>
+        <button className='text-newblue-950 flex flex-row border-b-4 border border-yellowneon-500 bg-yellowneon-500 drop-shadow-5xl p-2 rounded-lg justify-evenly md:w-24 w-20 cursor-pointer'>
           <Image src={back} alt='back-arrow'/>
           Back
         </button>
