bb7cd11868ff133ba0948f9e691a14278425b47f
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-08-02 13:22:14 -0300

    style: it fixes lint error 4

--- src/app/contexts/search-context.tsx
+++ src/app/contexts/search-context.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
 'use client'
 import { ReactNode, createContext, useState } from 'react'
 
